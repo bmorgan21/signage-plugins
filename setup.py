f56ea4f660b83188dba094a62d837e527a8230a7
Brian Morgan <brian.s.morgan@gmail.com>
2015-12-15 13:14:05 -0500

    rename stuff

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version='0.1.0',
     author='Brian S Morgan',
     author_email='brian.s.morgan@gmail.com',
-    packages=['signage-plugins'],
-    url='https://github.com/bmorgan21/signage-plugins',
+    packages=['signage_plugins'],
+    url='https://github.com/bmorgan21/signage_plugins',
     description='Integrations for pulling and publishing data to node signage-server.',
     install_requires=[
         'Jinja2>=2.8',
